33143933822130fd9cc28210cd9b1f95dab0b5e4
Jacek <jacek_kozakowski_dev@wp.pl>
2023-11-24 20:51:37 +0100

    Figures algorithm

--- chess.js
+++ chess.js
@@ -1,39 +1,36 @@
 let whitePlayer = [];
 let blackPlayer = [];
 
+function createFigure(nName, nSide, nX, nY) {
+  whitePlayer.push({
+    name: nName,
+    side: nSide,
+    active: true,
+    position: {
+      x: nX,
+      y: nY
+    }
+  });
+}
 
 function displayfigure(figure) {
   const chessBoard = document.querySelector(`.${(figure.position.x)}-${figure.position.y}`);
   let newFigure = document.createElement('div');
   newFigure.classList.add(figure.name);
   newFigure.classList.add(figure.side);
-  newFigure.innerHTML = `<img src="./images/${figure.side}-pawn.png">`;
+  newFigure.innerHTML = `<img src="./images/${figure.side}-${figure.name}.png">`;
   chessBoard.appendChild(newFigure);
+  console.log(newFigure)
 }
 
 function setChess() {
   for(let i = 'A'.charCodeAt(); i <= 'H'.charCodeAt(); i++) {
-    whitePlayer.push({
-      name: 'pawn',
-      side: 'white',
-      active: true,
-      position: {
-        x: String.fromCharCode(i),
-        y: 2
-      }
-    });
+    createFigure('pawn', 'white', String.fromCharCode(i), 2);
   }
   for(let i = 'A'.charCodeAt(); i <= 'H'.charCodeAt(); i++) {
-    blackPlayer.push({
-      name: 'pawn',
-      side: 'black',
-      active: true,
-      position: {
-        x: String.fromCharCode(i),
-        y: 7
-      }
-    });
+    createFigure('pawn', 'black', String.fromCharCode(i), 7);
   }
+  createFigure('quinn', 'white', 'E', 1);
 
   for(let i = 0; i < whitePlayer.length; i++) {
     displayfigure(whitePlayer[i]);
