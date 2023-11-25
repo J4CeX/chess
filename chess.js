b38c29d000cfc56e6c6e7b849d71c36608a4ee3e
Jacek <jacek_kozakowski_dev@wp.pl>
2023-11-25 14:27:56 +0100

    Chess setStart

--- chess.js
+++ chess.js
@@ -18,9 +18,9 @@ function displayfigure(figure) {
   let newFigure = document.createElement('div');
   newFigure.classList.add(figure.name);
   newFigure.classList.add(figure.side);
+  newFigure.classList.add('figure')
   newFigure.innerHTML = `<img src="./images/${figure.side}-${figure.name}.png">`;
   chessBoard.appendChild(newFigure);
-  console.log(newFigure)
 }
 
 function setChess() {
@@ -31,7 +31,24 @@ function setChess() {
     createFigure('pawn', 'black', String.fromCharCode(i), 7);
   }
   createFigure('quinn', 'white', 'E', 1);
-  
+  createFigure('quinn', 'black', 'E', 8);
+  createFigure('hetman', 'white', 'D', 1);
+  createFigure('hetman', 'black', 'D', 8);
+
+  createFigure('rook', 'white', 'A', 1);
+  createFigure('rook', 'white', 'H', 1);
+  createFigure('rook', 'black', 'A', 8);
+  createFigure('rook', 'black', 'H', 8);
+
+  createFigure('knight', 'white', 'B', 1);
+  createFigure('knight', 'white', 'G', 1);
+  createFigure('knight', 'black', 'B', 8);
+  createFigure('knight', 'black', 'G', 8);
+
+  createFigure('bishop', 'white', 'C', 1);
+  createFigure('bishop', 'white', 'F', 1);
+  createFigure('bishop', 'black', 'C', 8);
+  createFigure('bishop', 'black', 'F', 8);
 
   for(let i = 0; i < whitePlayer.length; i++) {
     displayfigure(whitePlayer[i]);
